446de47530b8da430801d3bb06b20cdf38ff8be5
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-03-15 15:42:18 +0100

    Enabled the form for searching

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    console.log(event);
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
-      <form>
-        <input type="search" />
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
       </form>
     </div>
   );
