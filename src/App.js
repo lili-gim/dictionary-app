d1138899ab410e26671e201bc83921b8f9601a2f
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-03-15 15:09:19 +0100

    Created Dictionary components and added a form.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <Dictionary />
     </div>
   );
 }
