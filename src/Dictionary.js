d0495bf129666a62b281a6d8214fbcb6a194dbb2
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-03-16 17:15:51 +0100

    Created Result component.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Result from "./Result";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
@@ -30,6 +31,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Result />
     </div>
   );
 }
