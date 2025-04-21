b583a2a1d525aa03f30f1f60c42f62fb2e5ba090
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-04-21 16:13:21 +0200

    Restructured functions to add default search result

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <Dictionary />
+      <Dictionary defaultKeyword="word" />
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,10 +3,11 @@ import "./Dictionary.css";
 import axios from "axios";
 import Result from "./Result";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [phonetic, setPhonetics] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     //console.log(response.data);
@@ -14,9 +15,7 @@ export default function Dictionary() {
     setPhonetics(response.data.phonetic);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     //documentation: https://www.shecodes.io/learn/apis/dictionary
 
     let apiKey = "c20161a98fff44a06b23236e2obd0t3b";
@@ -25,20 +24,35 @@ export default function Dictionary() {
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     //console.log(event);
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange} />
-        </form>
-        <div className="hint">e.g. cat, flower, moon</div>
-      </section>
-      <Result result={results} phonetic={phonetic} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint">e.g. word, flower, moon</div>
+        </section>
+        <Result result={results} phonetic={phonetic} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
