fa2e31e7905cd8cb7f4d390a48178b219c1d43bc
lili-gim <156603091+lili-gim@users.noreply.github.com>
2025-04-21 17:42:57 +0200

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./abc-logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <img src={logo} className="App-logo" alt="logo" />
       <Dictionary defaultKeyword="word" />
     </div>
   );
